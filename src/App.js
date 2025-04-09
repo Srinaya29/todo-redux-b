9a7b3d1b173b690c730c6fcdfe6a92cbd18fc727
nairx <nairxprv@gmail.com>
2025-04-09 13:57:21 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Todo from "./features/todo/Todo";
 import User from "./features/user/User";
+import Login from "./features/user/Login";
+import Header from "./features/header/Header"
+import Footer from "./features/footer/Footer"
+import Home from "./features/home/Home"
 const App = () => {
   return (
     <>
-      <User />
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 };
