63c9a35bf3a2da07fca89fafd248caaf674634d7
Srinaya29 <sydugarisrinaya@gmail.com>
2025-04-10 22:08:42 +0530

    added

--- src/features/user/User.js
+++ src/features/user/User.js
@@ -1,55 +1,64 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./User.css";
 import { useSelector, useDispatch } from "react-redux";
 import { addUser } from "./userSlice";
 
 export default function User() {
-  const [userObj, setUserObj] = useState({});
+  const [userObj, setUserObj] = useState({ name: "", email: "", pass: "" });
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users);
+
   const handleAddUser = () => {
-    dispatch(addUser(userObj));
+    const { name, email, pass } = userObj;
+
+    if (!name || !email || !pass) {
+      alert("Please fill all the fields");
+      return;
+    }
+
+    const newUser = {
+      ...userObj,
+      id: Date.now(),
+    };
+
+    dispatch(addUser(newUser));
+    setUserObj({ name: "", email: "", pass: "" }); // Clear form
   };
+
   return (
     <div className="App-Register-Row">
       <div>
         <h2>Registration Form</h2>
-        <p>
-          <input
-            type="text"
-            onChange={(e) => setUserObj({ ...userObj, name: e.target.value })}
-            placeholder="Enter name"
-          ></input>
-        </p>
-        <p>
-          <input
-            type="text"
-            onChange={(e) => setUserObj({ ...userObj, email: e.target.value })}
-            placeholder="Email address"
-          ></input>
-        </p>
-        <p>
-          <input
-            type="password"
-            onChange={(e) => setUserObj({ ...userObj, pass: e.target.value })}
-            placeholder="New password"
-          ></input>
-        </p>
-        <p>
-          <button onClick={handleAddUser}>Submit</button>
-        </p>
+        <input
+          type="text"
+          value={userObj.name}
+          onChange={(e) => setUserObj({ ...userObj, name: e.target.value })}
+          placeholder="Enter name"
+        />
+        <input
+          type="email"
+          value={userObj.email}
+          onChange={(e) => setUserObj({ ...userObj, email: e.target.value })}
+          placeholder="Email address"
+        />
+        <input
+          type="password"
+          value={userObj.pass}
+          onChange={(e) => setUserObj({ ...userObj, pass: e.target.value })}
+          placeholder="New password"
+        />
+        <button onClick={handleAddUser}>Submit</button>
       </div>
       <div>
         <h3>User List</h3>
         <ol>
-        {users &&
-          users.map((value) => (
-            <li key={value.id}>
-              {value.name}-{value.email}-{value.pass}
-            </li>
-          ))}
-          </ol>
+          {users &&
+            users.map((value) => (
+              <li key={value.id}>
+                <strong>{value.name}</strong> - {value.email} - {value.pass}
+              </li>
+            ))}
+        </ol>
       </div>
     </div>
   );
